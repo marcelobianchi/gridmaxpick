0333cd9dd5337a787db9d5905d1f9b1e49fcff77
Marcelo Bianchi <m.bianchi@iag.usp.br>
2014-12-09 21:30:08 -0200

    multiple grids in sh files

--- conv.sh
+++ conv.sh
@@ -1,12 +1,15 @@
 #/bin/bash
 
-G=x.grd
+G=$1
 
 #Here you set the limits for plotting and resolution
-#grdsample -Gn $G -I0.05/2 -R-2.0/0.3/-200/200
-cp x.grd xn.gdr
+
+#grdsample -Gn $G -I0.02/0.2 -R-2.0/0.3/-200/200
+#cp n xn.gdr
+
+cp -iv $G xn.grd
+grdinfo -C xn.grd
 
 #Here we export to ASCII
-grdinfo -C xn.gdr | awk '{print $2,$3,$8,$10"\n"$4,$5,$9,$11}' > _D
-grd2xyz -Z xn.gdr >> _D 
-
+grdinfo -C xn.grd | awk '{print $2,$3,$8,$10"\n"$4,$5,$9,$11}' > _D
+grd2xyz -ZBLa xn.grd >> _D 
--- plotx.sh
+++ plotx.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 
-makecpt -Cgray -T0/1/0.002 > la.cpt
-grdimage x.grd -JX26/18 -B0.5/50a100 -Cla.cpt -Xc -Yc > la.ps
+##makecpt -Cgray -T0/1/0.002 > la.cpt
+makecpt -Cgray -T-2/2/0.2 > la.cpt
+
+grdimage $1 -JX26/18 -B0.5/50a100 -Cla.cpt -Xc -Yc > la.ps
 
 gv la.ps
 
