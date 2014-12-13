e001e4d1c4906bd2e6011272093fbc356be597a6
Marcelo Bianchi <m.bianchi@iag.usp.br>
2014-12-13 20:39:10 -0200

    New conv

--- conv.sh
+++ conv.sh
@@ -2,14 +2,28 @@
 
 G=$1
 
-#Here you set the limits for plotting and resolution
+## Initialize
+outf=_D
+tmp=$(mktemp)
+cp -v "$G" "$tmp"|| exit
 
-#grdsample -Gn $G -I0.02/0.2 -R-2.0/0.3/-200/200
-#cp n xn.gdr
+## 
+# Here you set the limits for plotting and resolution
+#
+#grdsample -Gn ${tmp} -I0.02/0.2 -R-2.0/0.3/-200/200; mv n ${tmp}
 
-cp -iv $G xn.grd
-grdinfo -C xn.grd
+echo ""
+echo "Some info for the mind"
+echo ""
+grdinfo -C ${tmp}
 
-#Here we export to ASCII
-grdinfo -C xn.grd | awk '{print $2,$3,$8,$10"\n"$4,$5,$9,$11}' > _D
-grd2xyz -ZBLa xn.grd >> _D 
+# HEADER
+echo ""
+echo "HEADER -> ${outf}"
+grdinfo -C ${tmp} | awk '{print $2,$3,$8,$10"\n"$4,$5,$9,$11}' > ${outf}
+echo "DATA -> ${outf}"
+grd2xyz -ZBLa ${tmp} >> ${outf}
+echo "DONE, file is ${outf}"
+
+# CLEAN UP
+rm -f ${tmp}
