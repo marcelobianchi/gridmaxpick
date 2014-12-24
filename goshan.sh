a9355b9ef4e79c32465d5d2909cf07843e5b7818
Marcelo Bianchi <m.bianchi@iag.usp.br>
2014-12-24 11:17:13 -0200

    improved ? goshan

--- goshan.sh
+++ goshan.sh
@@ -1,22 +1,31 @@
 #!/bin/bash 
 
 # If gmt.h file is in /usr/include/gmt/gmt.h add here /usr/include
+#
 GMT="/opt/local/lib/gmt4/include"
+GMT="/usr/include/gmt"
 
 # The folder where libgmt.so or in the mac, the dynamic file.
+#
 GMTLIB="/opt/local/lib/gmt4/lib"
 
-[ -z "$GMT" ] && echo "EDIT ME " && exit
+# Checks
+#
+[ ! -f "$GMT/gmt.h" ] && echo "No file $GMT/gmt.h" && exit
+[ ! -f "$GMTLIB/libgmt.dynlib" ] && echo "No file $GMTLIB/libgmt.dynlib" && exit
 
 # Compile
-gcc -c -m64 -pipe -O2 -Wall -W -DUSEGMTGRD -DQT_WEBKIT -I/usr/share/qt4/mkspecs/linux-g++-64 -I. -I$GMT -I/opt/local/include -o main.o main.c
-gcc -c -m64 -pipe -O2 -Wall -W -DUSEGMTGRD -DQT_WEBKIT -I/usr/share/qt4/mkspecs/linux-g++-64 -I. -I$GMT -I/opt/local/include -o interaction.o interaction.c
-g++ -m64 -o testegrid main.o interaction.o  -lcpgplot  -L/opt/local/lib/ -L$GMTLIB -lm -lgmt
+#
+gcc -c -m64 -O2 -W -DUSEGMTGRD -I. -I${GMT} -I/opt/local/include -o main.o main.c
+gcc -c -m64 -O2 -W -DUSEGMTGRD -I. -I${GMT} -I/opt/local/include -o interaction.o interaction.c
+gcc -o testegrid main.o interaction.o  -lcpgplot  -L/opt/local/lib/ -L${GMTLIB} -lm -lgmt
 
 # VARIABLES
-export LD_LIBRARY_PATH=/opt/local/lib:$LD_LIBRARY_PATH
+#
+export LD_LIBRARY_PATH=/opt/local/lib:/opt/local/lib/gmt4/lib:$LD_LIBRARY_PATH
 export PGPLOT_DIR=/opt/local/share/pgplot/
 export PATH=/opt/local/bin:$PATH
 
 # RUN
+#
 ./testegrid
